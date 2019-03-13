8cdb1a19a76058ec93d7307afb42b767d0119ce9
Juan Daniel Pichardo <jpichardopena@gmail.com>
2019-03-13 00:06:25 -0400

    Set auth module state, getters and mutations

---
+++ src/store/modules/auth.js
@@ -0,0 +1,17 @@
+const state = {
+  token: null
+};
+
+const getters = {
+  isLoggedIn: state => !!state.token
+};
+
+const actions {
+
+};
+
+const mutations = {
+  setToken: (state, token) => {
+    state.token = token
+  }
+};
