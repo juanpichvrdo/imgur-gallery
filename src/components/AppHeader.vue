1f1d0734c3f71ece9c3a6da2efc303e5614f0e80
Juan Daniel Pichardo <jpichardopena@gmail.com>
2019-03-13 22:05:40 -0400

    Add logout functionality and nav buttons styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,10 +3,10 @@
     <a href="/" class="active item">Image Storage</a>
 
     <div class="right menu">
-      <div v-if="isLoggedIn">
-        Gallery Button
-        Upload Button
-        Logout Button
+      <div v-if="isLoggedIn" class="horizontal">
+        <a class="item">Galleries</a>
+        <a class="item">Upload</a>
+        <a class="item" @click="logout">Logout</a>
       </div>
       <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
@@ -19,9 +19,13 @@ import { mapActions, mapGetters } from "vuex";
 export default {
   name: "AppHeader",
   computed: mapGetters(["isLoggedIn"]),
-  methods: mapActions(["login"])
+  methods: mapActions(["login", "logout"])
 };
 </script>
 
 <style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
 </style>
