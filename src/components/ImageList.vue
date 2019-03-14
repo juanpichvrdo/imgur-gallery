b3d228b763fb7b75289a65eb67b3c016abcce32b
Juan Daniel Pichardo <jpichardopena@gmail.com>
2019-03-14 01:02:35 -0400

    Add images modules

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -3,8 +3,14 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  name: "ImageList"
+  name: "ImageList",
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  }
 };
 </script>
 
---
+++ src/store/modules/images.js
@@ -0,0 +1,31 @@
+import api from "../../api/imgur";
+
+const state = {
+  images: []
+};
+
+const getters = {
+  allImages: state => state.images
+};
+
+const actions = {
+  async fetchImages({ rootState }) {
+    const { token } = rootState.auth;
+
+    const response = await api.fetchImages(token);
+    console.log(response);
+  }
+};
+
+const mutations = {
+  setImages: (state, images) => {
+    state.images = images;
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
