897a4f23607e6d9fc2396db3edd9a5eebb222bed
Juan Daniel Pichardo <jpichardopena@gmail.com>
2019-03-14 22:51:48 -0400

    Add image list styling and message when not authenticated

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <img v-for="image in allImages" :src="image.link" :alt="image.description" :key="image.id">
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages" :src="image.link" :alt="image.description" :key="image.id">
+    </div>
+    <h2 v-else>Log in to get started</h2>
   </div>
 </template>
 
@@ -9,7 +12,7 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
-  computed: mapGetters(["allImages"]),
+  computed: mapGetters(["allImages", "isLoggedIn"]),
   methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
@@ -17,5 +20,14 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+
+img {
+  max-width: 100%;
+  padding: 5px;
+}
 </style>
